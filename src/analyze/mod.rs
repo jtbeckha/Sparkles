f71be46db7fd985605c11158109c044a43132a67
Jared <jared@jaredbeckham.com>
2019-06-18 19:51:48 -0400

    Adding Frame struct to hold stereo PCM data and functions to handle stereo data

--- src/analyze/mod.rs
+++ src/analyze/mod.rs
@@ -1,3 +1,5 @@
+use crate::decode::Frame;
+
 ///! The analyze module contains functions for analysis of PCM data to convert it into something
 ///! interesting to visualize e.g. RMS amplitude, frequency spectrum, etc.
 
@@ -10,4 +12,20 @@ pub fn rms_amplitude(samples: &mut Vec<f32>) -> f32 {
 
     let rms = (square_sum / samples.len() as f32).sqrt();
     return rms
+}
+
+/// Compute RMS amplitude of a set of stereo samples and returns the value for the left and
+/// right channels, respectively.
+pub fn rms_amplitude_stereo(samples: &mut Vec<Frame>) -> (f32, f32) {
+    let mut left_square_sum = 0f32;
+    let mut right_square_sum = 0f32;
+    for (_, elem) in samples.iter().enumerate() {
+        left_square_sum = left_square_sum + elem.left.powf(2f32);
+        right_square_sum = right_square_sum + elem.right.powf(2f32);
+    }
+
+    let left_rms = (left_square_sum / samples.len() as f32).sqrt();
+    let right_rms = (right_square_sum / samples.len() as f32).sqrt();
+
+    return (left_rms, right_rms);
 }
--- src/decode.rs
+++ src/decode.rs
@@ -2,6 +2,12 @@ use byteorder::{ByteOrder, NativeEndian};
 
 const BYTES_PER_SAMPLE: usize = 4;
 
+/// A frame of stereo audio data containing an f32 PCM sample for the left and right channel.
+pub struct Frame {
+    pub left: f32,
+    pub right: f32
+}
+
 /// Decode audio stream data into a vector of f32 PCM samples.
 pub fn decode(input: &[u8]) -> Vec<f32> {
     // TODO make sure this will always be true (i.e. input should be constrained to a number divisible by BYTES_PER_SAMPLE)
@@ -13,6 +19,26 @@ pub fn decode(input: &[u8]) -> Vec<f32> {
         assert_eq!(BYTES_PER_SAMPLE, chunk.len());
         let sample = NativeEndian::read_f32(&chunk);
         output.push(sample);
+    }
+    return output;
+}
+
+/// Decode audio stream data into a vector of Frames.
+pub fn decode_stereo(input: &[u8]) -> Vec<Frame> {
+    // TODO make sure this will always be true (i.e. input should be constrained to a number divisible by BYTES_PER_SAMPLE)
+    // so that we never mix up stereo channels which are interleaved in the resulting sample Vec.
+    let vec_capacity = input.len() / BYTES_PER_SAMPLE;
+    assert_eq!(0, input.len() / BYTES_PER_SAMPLE % 2);
+    let mut output = Vec::with_capacity(vec_capacity);
+    for chunk in input.chunks_exact(BYTES_PER_SAMPLE * 2) {
+        assert_eq!(BYTES_PER_SAMPLE * 2, chunk.len());
+        let (left_bytes, right_bytes) = chunk.split_at(BYTES_PER_SAMPLE);
+
+        let left = NativeEndian::read_f32(left_bytes);
+        let right = NativeEndian::read_f32(right_bytes);
+        output.push(Frame {
+            left, right
+        });
     }
     return output;
 }
